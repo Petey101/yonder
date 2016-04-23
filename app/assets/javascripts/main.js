7dac1a840be466a0e15362aaa9b20ce8fcd073c2
Miguel Melendez <m.melend@gmail.com>
2016-04-23 15:41:25 -0400

    no changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,3 +11,24 @@ $( document).ready(function(){
 	})
 
 })
+
+function getDegrees(lat1, long1, lat2, long2, headX) {
+
+    var dLat = toRad(lat2-lat1);
+    var dLon = toRad(lon2-lon1);
+
+    lat1 = toRad(lat1);
+    lat2 = toRad(lat2);
+
+    var y = Math.sin(dLon) * Math.cos(lat2);
+    var x = Math.cos(lat1)*Math.sin(lat2) -
+            Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
+    var brng = toDeg(Math.atan2(y, x));
+
+    // fix negative degrees
+    if(brng<0) {
+        brng=360-Math.abs(brng);
+    }
+
+    return brng - headX;
+}
