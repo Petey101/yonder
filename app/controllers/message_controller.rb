499ae727aeb9400355e9f9bd78b7738737728d48
Steven King <stevenking86@gmail.com>
2016-04-24 10:12:31 -0400

    Get generic response

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,10 +1,33 @@
 class MessageController < ApplicationController
+  skip_before_filter :verify_authenticity_token
 
   def receive_sms
-    content_type 'text/xml'
+    boot_twilio
+    sms = @client.messages.create(
+      from: +16788418305,
+      to: params["From"],
+      body: "HI!")
 
-    response = Twilio::TwiML::Response.new do |r|
-      r.message "This is working"
-    end
   end
+
+  private
+   def boot_twilio
+    account_sid = "AC17942be75d5e9d315c1b7fc261258605"
+    auth_token = "7572027dbfcb8d906abe23eb442364e1"
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
+  end
+
 end
+
+
+
+
+
+
+    # content_type 'text/xml'
+
+    # response = Twilio::TwiML::Response.new do |r|
+    #   r.Message "This is working"
+    # end
+
+    # response.to_xml
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,18 @@
 class Message < ActiveRecord::Base
+  # private
+
+  # def boot_twilio
+  #   account_sid = "AC17942be75d5e9d315c1b7fc261258605"
+  #   auth_token = "7572027dbfcb8d906abe23eb442364e1"
+  #   @client = Twilio::REST::Client.new(account_sid, auth_token)
+  # end
+
+
+
+
+    # sms = @client.messages.create(
+    #   from: +16788418305,
+    #   to: params["From"],
+    #   body: "HI!")
+
 end
