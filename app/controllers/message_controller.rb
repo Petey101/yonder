3c6cc1cad686fbee30bb2159dff2e930f17c7865
Steven King <stevenking86@gmail.com>
2016-04-24 11:32:32 -0400

    Get water source in ruby

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -10,6 +10,15 @@ class MessageController < ApplicationController
 
   end
 
+  def get_water_string
+  end
+
+  def get_weather_string
+  end
+
+  def get_info
+  end
+
   private
    def boot_twilio
     account_sid = "AC17942be75d5e9d315c1b7fc261258605"
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,18 +1,46 @@
 class Message < ActiveRecord::Base
-  # private
 
-  # def boot_twilio
-  #   account_sid = "AC17942be75d5e9d315c1b7fc261258605"
-  #   auth_token = "7572027dbfcb8d906abe23eb442364e1"
-  #   @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+  def self.get_water_locations(lat, lon)
+    url = "https://data.waterpointdata.org/resource/gihr-buz6.json?$where=within_circle(location, #{lat}, #{lon}, 5000)"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    #not actually the closest source...
+    return JSON.parse(response)[0]
+
+  end
+
+  def calc_distance_between(lat1, lon1, lat2, lon2)
+     radius = 3958.7558657440545
+     d_lat = to_rad(lat2-lat1)
+     d_lon = to_rad(lon2-lon1)
+     a = Math.sin(d_lat/2) * Math.sin(d_lat/2) +
+             Math.cos(to_rad(lat1)) * Math.cos(to_rad(lat2)) *
+             Math.sin(d_lon/2) * Math.sin(d_lon/2)
+     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+     d = radius * c
+  end
+
+  def to_rad(value)
+   return value * Math::PI / 180
+  end
+
+
+  # def self.closest_source(lat, lon)
+  #   locations = self.get_water_locations(lat,lon)
+  #   sorted_locations = []
+  #   locations.each do |location|
+  #     puts location
+  #     sorted_locations.push({source: location, distance: calc_distance_between(location["lat_deg"].to_f,location["lon_deg"].to_f, lat.to_f, lon.to_f)})
+  #   end
+  #   sorted_locations.sort_by { |location| location.distance}
+  #   return sorted_locations
   # end
 
+  def get_weather_string
+  end
 
-
-
-    # sms = @client.messages.create(
-    #   from: +16788418305,
-    #   to: params["From"],
-    #   body: "HI!")
+  def get_info
+  end
 
 end
